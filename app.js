dbe7d62fa926583c39cbc0f53a97caacff2dbbfb
REKHAE JAVALI <rekhajavali22@gmal.com>
2023-02-18 23:48:52 +0530

    final commit without deployment to server

--- app.js
+++ app.js
@@ -3,8 +3,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose =  require("mongoose");
 
 const _ = require("lodash");
+mongoose.set('strictQuery', true);
+mongoose.connect("mongodb+srv://Rekha:Rekha13@cluster0.uihvcdf.mongodb.net/blogpostsDB");
+
+const blogSchema = new mongoose.Schema({
+  title : String,
+  content: String
+});
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -17,10 +29,14 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-let posts = [];
+// let posts = []; will add to db
 
 app.get("/", function(req, res){
-  res.render("home", {homeContent :homeStartingContent, numofpost:posts});
+
+  Blog.find({},function(err, foundposts){
+    res.render("home", {homeContent :homeStartingContent, numofpost:foundposts});
+  });
+ 
   // // res.send(homeStartingContent);  
   // console.log(posts);
   // for (var i = 0; i<posts.length; i++)
@@ -36,19 +52,32 @@ app.get("/contact" , function(req, res){
 });
 
 app.get("/compose" , function(req, res){
+
   res.render("compose");
+
 
 
 });
 
 app.post("/compose", function(req,res){
-  const post ={
+ /* const post ={
     title:req.body.titleText,
     content: req.body.postBody
-  };
-  
-  posts.push(post);
-  res.redirect("/");
+  };*/
+
+//add to db:
+  const post = new Blog({
+    title:req.body.titleText,
+    content: req.body.postBody
+  })
+
+  post.save(function(err){
+    if(!err)
+    res.redirect("/");
+  });
+
+  // posts.push(post);
+  // res.redirect("/");
   // console.log(posts);
   // console.log("title: "+post.title);
   // console.log("post: "+post.content);
@@ -56,26 +85,36 @@ app.post("/compose", function(req,res){
 
 let storedbody = '';
 //routing paramaters
-app.get("/posts/:topic" , function(req, res){
-  const topictitle = _.lowerCase(req.params.topic); //entered in compose/posts/.....
-  console.log(topictitle);
+app.get("/posts/:topicid" , function(req, res){
+  // const topictitle = _.lowerCase(req.params.topic);
+  const requestedpostid = req.params.topicid;  //entered in compose/posts/.....
+  // console.log(topictitle);
   // for (var i = 0; i<posts.length; i++){
-    posts.forEach(function(post){
+    /*posts.forEach(function(post){
     // console.log(posts[i].title);
     const storedPostTitle = _.lowerCase(post.title);//stored in home after entered in compose page
     storedbody = post.content;
-    console.log(storedPostTitle);
+    console.log(storedPostTitle);*/
     // if(posts[i].title == topictitle){
-      if (storedPostTitle === topictitle){
-      console.log("match found");
+
+    Blog.findOne({_id:requestedpostid},function(err, foundpost){
+      if(!err){
       res.render("post" ,
-      { postTitle :post.title, postcontent : post.content });
-    }
-    else{
-      console.log("not found");
-    }
+      { postTitle :foundpost.title, postcontent : foundpost.content });
+    }else{
+           console.log("not found", err);
+       }
+    });
+  //     if (storedPostTitle === topictitle){
+  //     console.log("match found");
+  // res.render("post" ,
+  //     { postTitle :post.title, postcontent : post.content });
+      
+  //   else{
+  //     console.log("not found");
+  //   }
   
-  });
+  // });
   
 });
 
